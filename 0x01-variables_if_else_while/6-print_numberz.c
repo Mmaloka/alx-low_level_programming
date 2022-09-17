de918d9beee313c1f186ea09dd410f74ce147b5f
Mmaloka <malengoabala@gmail.com>
2022-09-17 02:32:16 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 0;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
