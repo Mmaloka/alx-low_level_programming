c4cb650ade09a497e449ccf3c2f2e8d15f3a4701
Mmaloka <malengoabala@gmail.com>
2022-09-17 03:12:01 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+	char n = 'a';
+
+	for (num = 0; num <= 9, num++)
+	{
+		putchar(num + '0');
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
