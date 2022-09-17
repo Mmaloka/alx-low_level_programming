7422075c26e15d3f4d128af68fe5edab43965c00
Mmaloka <malengoabala@gmail.com>
2022-09-17 13:12:15 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  * main - main entry
  * Return: 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char c = 'a';
 
@@ -13,5 +12,4 @@ void print_alphabet(void);
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
